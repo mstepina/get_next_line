7854c296fc9416a81e0f4bfc5c51ced1d587a3a4
mstepina <50503381+mstepina@users.noreply.github.com>
2019-06-23 20:07:34 -0700

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,6 +12,12 @@
 
 #include "get_next_line.h"
 
+*/
+** Finds a new line character. Gets the pointer to the line
+** (up to a new line character) with ft_strsub function from libft library. 
+** Stores a pointer to the first character after new line character
+** in a static array.
+*/
 void	n_check(int fd, char **line, char **str)
 {
 	int		i;
@@ -31,12 +37,20 @@ void	n_check(int fd, char **line, char **str)
 	}
 }
 
+*/
+** Gets the pointer to the last line in the file.
+*/
+
 void	end_check(int fd, char **line, char **str)
 {
 	*line = ft_strdup(str[fd]);
 	ft_strdel(&str[fd]);
 }
 
+
+*/
+** Function checks the line for a new line character or the end of the file  
+*/
 void	checker(int fd, char **line, char **str)
 {
 	int i;
@@ -51,7 +65,10 @@ void	checker(int fd, char **line, char **str)
 }
 
 */ 
-** 
+** Reads the file of BUFF_SIZE characters. If str array for this file descriptor is empty,
+** it copies the content of the buffer into it. Otherwise, with the help of ft_strjoin function
+** it adds the content of the buffer to the end of the fd element of str array. 
+** The reading stops if there is a new line character
 */
 
 int		read_func(int fd, char **str, int res)
@@ -79,8 +96,10 @@ int		read_func(int fd, char **str, int res)
 */
 ** First, the function checks for errors (invalid file descriptor, empty line or buffer size less than 1).
 ** If there are any, returns -1. 
-** Then it reads the size of the buffer from the file. If reading fails, returns -1. 
-** 
+** Then it calls read_func, that reads BUFF_SIZE from the file. If reading fails, returns -1. 
+** If there are no new line characters in the file, returns 0. 
+** If there is at least one new line in the file, but the file does not end with a new line character,
+** the function should return this line too (see recursive call of get_next_line function).
 */
 
 int		get_next_line(const int fd, char **line)
